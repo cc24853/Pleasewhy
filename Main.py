927c6aa01ec9d730ea12baab9d5158b9705988b2
cc24853 <cc24853@email.vccs.edu>
2018-03-05 20:49:13 -0500

    Added a lot of test text.

--- Main.py
+++ Main.py
@@ -1,5 +1,11 @@
 def main():
-    print('hello world')
+    print('hello world. I once met a traveler from an antique land who said: two vast and trunkless legs'
+          'of stone stand in the desert. Near them, in the sand,  Half sunk a shattered visage remains, whos frown, and'
+          'wrinkled lip, and sneer of cold command, tell that its sculptor well those passions read, which yet survive'
+          'stamped on these lifeless things. the hand that mocked them and the heart that fed. And on the pedestal,'
+          'these words appear: My name is Ozymandias, king of kings. Look upon my work ye mighty and dispair. Nothing'
+          'beside remains ere the decay of that colassal wreck. Boundless and bare the lone and level sands stretch'
+          'far away. ')
 
 
 if __name__ == '__main__':
